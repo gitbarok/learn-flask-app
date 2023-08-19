2193e08e16f4b112abaf2588de567cbe4146e66b
gitbarok <muhammadrizkymubarok@gmail.com>
2023-08-19 14:56:11 +0800

    add json file as db

--- app.py
+++ app.py
@@ -1,40 +1,47 @@
 from flask import Flask, redirect, render_template, request, url_for
 from forms import LoginForm, RegistrationForm
+import json
 
 # from forms import LoginForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "rahasia"
 
-data_dosen = {
-    1: {"Nama": "John Doe", "NIP": "1234", "Status": "Available"},
-    2: {"Nama": "John Doe", "NIP": "1", "Status": "Available"},
-    3: {"Nama": "John Doe", "NIP": "2", "Status": "Not Available"},
-    4: {"Nama": "John Doe", "NIP": "13", "Status": "Not Available"},
-}
+with open("data_dosen.json", "r") as json_file:
+    data_dosen = json.load(json_file)
 
 
 @app.route("/")
 def home():
+    with open("data_dosen.json", "r") as json_file:
+        data_dosen = json.load(json_file)
     return render_template("home.html", title="Home", data_dosen=data_dosen)
 
 
 @app.route("/dashboard", methods=["POST", "GET"])
 def dashboard():
+    with open("data_dosen.json", "r") as json_file:
+        data_dosen = json.load(json_file)
     return render_template("dashboard.html", data_dosen=data_dosen, title="Dashboard")
 
 
 @app.route("/add_user", methods=["POST", "GET"])
 def add_user():
     form = RegistrationForm()
+    with open("data_dosen.json", "r") as json_file:
+        data_dosen = json.load(json_file)
     if form.is_submitted():
         new_data = {
             "Nama": form.username.data,
             "NIP": form.password.data,
             "Status": "Available",
         }
-        next_key = max(data_dosen.keys(), default=1) + 1
+        next_key = str(max(map(int, data_dosen.keys()), default=0) + 1)
         data_dosen[next_key] = new_data
+
+        with open("data_dosen.json", "w") as json_file:
+            json.dump(data_dosen, json_file, indent=4)
+
         return redirect(url_for("dashboard"))
     return render_template("add_user.html", form=form, title="add user")
 
